664c9c5ab6858f9145a86948aeb0cc414d97f9c6
akiyu33333 <aaaaaki35@gmail.com>
2020-03-16 22:53:56 +0900

    docを追加

---
+++ slim-rest-app-backend/docment/js/searchIndex.js
@@ -0,0 +1,44 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Tests\\Functional\\BaseTestCase",
+            "name": "BaseTestCase",
+            "summary": "This\u0020is\u0020an\u0020example\u0020class\u0020that\u0020shows\u0020how\u0020you\u0020could\u0020set\u0020up\u0020a\u0020method\u0020that\nruns\u0020the\u0020application.\u0020Note\u0020that\u0020it\u0020doesn\u0027t\u0020cover\u0020all\u0020use\u002Dcases\u0020and\u0020is\ntuned\u0020to\u0020the\u0020specifics\u0020of\u0020this\u0020skeleton\u0020app,\u0020so\u0020if\u0020your\u0020needs\u0020are\ndifferent,\u0020you\u0027ll\u0020need\u0020to\u0020change\u0020it.",
+            "url": "../classes/Tests-Functional-BaseTestCase.html"
+        },                {
+            "fqsen": "\\Tests\\Functional\\BaseTestCase\u003A\u003ArunApp\u0028\u0029",
+            "name": "runApp",
+            "summary": "Process\u0020the\u0020application\u0020given\u0020a\u0020request\u0020method\u0020and\u0020URI",
+            "url": "../classes/Tests-Functional-BaseTestCase.html#method_runApp"
+        },                {
+            "fqsen": "\\Tests\\Functional\\BaseTestCase\u003A\u003A\u0024withMiddleware",
+            "name": "withMiddleware",
+            "summary": "Use\u0020middleware\u0020when\u0020running\u0020application\u003F",
+            "url": "../classes/Tests-Functional-BaseTestCase.html#property_withMiddleware"
+        },                {
+            "fqsen": "\\Tests\\Functional\\HomepageTest",
+            "name": "HomepageTest",
+            "summary": "This\u0020is\u0020an\u0020example\u0020class\u0020that\u0020shows\u0020how\u0020you\u0020could\u0020set\u0020up\u0020a\u0020method\u0020that\nruns\u0020the\u0020application.\u0020Note\u0020that\u0020it\u0020doesn\u0027t\u0020cover\u0020all\u0020use\u002Dcases\u0020and\u0020is\ntuned\u0020to\u0020the\u0020specifics\u0020of\u0020this\u0020skeleton\u0020app,\u0020so\u0020if\u0020your\u0020needs\u0020are\ndifferent,\u0020you\u0027ll\u0020need\u0020to\u0020change\u0020it.",
+            "url": "../classes/Tests-Functional-HomepageTest.html"
+        },                {
+            "fqsen": "\\Tests\\Functional\\HomepageTest\u003A\u003AtestGetHomepageWithoutName\u0028\u0029",
+            "name": "testGetHomepageWithoutName",
+            "summary": "Test\u0020that\u0020the\u0020index\u0020route\u0020returns\u0020a\u0020rendered\u0020response\u0020containing\u0020the\u0020text\u0020\u0027SlimFramework\u0027\u0020but\u0020not\u0020a\u0020greeting",
+            "url": "../classes/Tests-Functional-HomepageTest.html#method_testGetHomepageWithoutName"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "../namespaces/default.html"
+        },                {
+            "fqsen": "\\Tests\\Functional",
+            "name": "Functional",
+            "summary": "",
+            "url": "../namespaces/tests-functional.html"
+        },                {
+            "fqsen": "\\Tests",
+            "name": "Tests",
+            "summary": "",
+            "url": "../namespaces/tests.html"
+        }            ]
+);
